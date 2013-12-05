1c542c16d35732ba0a06a3e473d53ce0426ab893
Dawenster <dstwen@gmail.com>
2013-12-05 15:50:15 -0800

    update /flights post to respond to json

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,10 @@
 class FlightsController < ApplicationController
   def create
-    puts "*" * 100
-    puts "I got yo stuff!"
-    puts params
+    respond_to do |format|
+      puts "*" * 100
+      puts "I got yo stuff!"
+      puts params
+      format.json { render :json => { "message" => "It's all good!" } }
+    end
   end
 end
