4bca60339f920cb1bd7793a9c64f74fbf3968680
Dawenster <dstwen@gmail.com>
2013-12-04 16:44:19 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FsYvrApi::Application.config.secret_key_base = '77e46207c7d3d8de2c02cf0103387cedf333d0cb29b21f91d064965aebabe1996cb3bd37b45ae0eba83cf6e2bd05be3283e7dfa940506570c8035e5d387dc65c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FsYvrApi::Application.config.session_store :cookie_store, key: '_fs_yvr_api_session'
