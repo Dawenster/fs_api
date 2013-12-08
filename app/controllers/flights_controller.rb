b9c9886a03e651c9e0ecc77405feecc1cb271eeb
Dawenster <dstwen@gmail.com>
2013-12-08 11:45:41 -0800

    remove unnecessary "from" parameters; fixed filter for return; fix destroy for yesterday's flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -24,33 +24,21 @@ class FlightsController < ApplicationController
       passed_params["sort"] == "Price" ? sort = "price ASC" : sort = "departure_time ASC"
 
       if passed_params["segment"] == "Going"
-        from = passed_params["from"]
-        from_where = from == 0 ? "departure_airport_id > ?" : "departure_airport_id = ?"
+        # from = passed_params["from"]
+        # from_where = from == 0 ? "departure_airport_id > ?" : "departure_airport_id = ?"
 
         to = passed_params["to"]
         to_where = to == 0 ? "arrival_airport_id > ?" : "arrival_airport_id = ?"
       else
-        @returning = true
+        # from = passed_params["to"]
+        # from_where = to == 0 ? "departure_airport_id > ?" : "departure_airport_id = ?"
 
-        if passed_params["to"] == "Any"
-          to_where = "departure_airport_id > ?"
-          to = 0
-        else
-          to_where = "departure_airport_id = ?"
-          to = Airport.find_by_name(passed_params["to"]).id
-        end
-
-        if passed_params["from"] == "Any"
-          from_where = "arrival_airport_id > ?"
-          from = 0
-        else
-          from_where = "arrival_airport_id = ?"
-          from = Airport.find_by_name(passed_params["from"]).id
-        end
+        to = passed_params["from"]
+        to_where = from == 0 ? "arrival_airport_id > ?" : "arrival_airport_id = ?"
       end
 
       if passed_params["dates"] == ""
-        start_date = Time.now - 1.year
+        start_date = Time.now - 1.hour
         end_date = Time.now + 1.year
       else
         dates = passed_params["dates"].split(" - ") 
@@ -59,9 +47,9 @@ class FlightsController < ApplicationController
       end
 
       if passed_params["type"] == "Epic"
-        flights = Flight.where("shortcut = ? AND cheapest_price > ? AND #{from_where} AND #{to_where} AND departure_time >= ? AND arrival_time <= ? AND epic = ?", true, 0, from, to, start_date, end_date, true).order(sort).paginate(:page => passed_params["page"], :per_page => 10)
+        flights = Flight.where("shortcut = ? AND cheapest_price > ? AND #{to_where} AND departure_time >= ? AND arrival_time <= ? AND epic = ?", true, 0, to, start_date, end_date, true).order(sort).paginate(:page => passed_params["page"], :per_page => 10)
       else
-        flights = Flight.where("shortcut = ? AND cheapest_price > ? AND #{from_where} AND #{to_where} AND departure_time >= ? AND arrival_time <= ?", true, 0, from, to, start_date, end_date).order(sort).paginate(:page => passed_params["page"], :per_page => 10)
+        flights = Flight.where("shortcut = ? AND cheapest_price > ? AND #{to_where} AND departure_time >= ? AND arrival_time <= ?", true, 0, to, start_date, end_date).order(sort).paginate(:page => passed_params["page"], :per_page => 10)
       end
 
       destinations = Flight.all.map{ |f| f.arrival_airport_id }.uniq
@@ -77,7 +65,6 @@ class FlightsController < ApplicationController
   def create
     respond_to do |format|
       if params[:password] == ENV['POST_PASSWORD']
-        Flight.all.each { |flight| flight.update_attributes(:new => false) }
         Flight.where(:pure_date => params[:date]).destroy_all
 
         flights = params[:flights]
@@ -108,7 +95,7 @@ class FlightsController < ApplicationController
           )
         end
 
-        Flight.where(:new => false).destroy_all
+        Flight.where("created_at < ?", Time.now - 1.day).destroy_all
 
         format.json { render :json => { "message" => "It's all good!" } }
       else
